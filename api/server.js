25506e38879b11b29b4c5476058acb0ef03448ee
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-10-24 16:59:43 -0500

    wip

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const helmet = require("helmet");
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use('/', (req, res, next) => {
+  res.send('<h1>Hey There<h1>')
+})
+
+// const userRouter = require("./users/router");
+// server.use("/api/users", userRouter);
+
+// const classRouter = require("./classes/router");
+// server.use("/api/classes", classRouter);
+
+// const authRouter = require("./auth/auth_router");
+// server.use("/api/auth", authRouter);
+
+server.use("*", (req, res, next) => {
+  console.log(`hitting${req.method} and ${req.baseUrl}`);
+  next({ status: 404, message: "not found" });
+});
+
+server.use((err, req, res) => {
+  res.status(err.status || 500).json({ message: `Horror: ${err.message}` });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-require("dotenv").config(); //allows to 'inject' fake enviorment variables
+require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-
-const server = express();
-
-server.use(express.json()); // teaches express to parse req.body
-server.use(cors());
-server.use(helmet());
+const server = require("./api/server");
 
 const { PORT } = require("./config");
 
-server.get("/", (req, res) => {
-  res.send(`
-      <h1>Lambda School is the best<h1>
-      `);
-});
-
-server.use("*", (req, res, next) => {
-  res.json({
-    message: "Lambda School is the best!",
-  });
-});
-
 server.listen(PORT, () => {
-  // heroku machine uses a different port
   console.log(`listening on ${PORT}`);
 });
